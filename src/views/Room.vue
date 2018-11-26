d703e001e1f3051e4e88284ab24cac3888ce90ad
uu4k <t.shimizu1115@gmail.com>
2018-11-26 15:21:38 +0900

    add room page.

---
+++ src/views/Room.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="room">
+    <h1>room: {{ $route.params.roomId }}</h1>
+    <div class="loading" v-if="loading">読み込み中...</div>
+    <div class="error" v-if="error">{{ error }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class Room extends Vue {}
+</script>
